47bbe4888467a15d3a4e84ab5d0d840928e2066c
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-02-28 21:59:28 -0300

    car moving with  keyboards

---
+++ main.js
@@ -0,0 +1,15 @@
+myCanvas.height = window.innerHeight;
+myCanvas.width = 200;
+
+const ctx = myCanvas.getContext("2d");
+const car = new Car(100, 500, 30, 50);
+
+animate();
+
+function animate() {
+  myCanvas.height = window.innerHeight;
+  car.update();
+  car.draw(ctx);
+
+  requestAnimationFrame(animate);
+}
