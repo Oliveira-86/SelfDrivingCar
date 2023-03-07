d0c0e5ae82627be03366a7624773e221a0dd40c8
oliveira-86-c1risk <egberto.duarte.14@gmail.com>
2023-03-07 15:06:09 -0300

    added road, and fixed the car in the center of the screen

--- main.js
+++ main.js
@@ -2,13 +2,18 @@ myCanvas.height = window.innerHeight;
 myCanvas.width = 200;
 
 const ctx = myCanvas.getContext("2d");
-const car = new Car(100, 500, 30, 50);
+const road = new Road(myCanvas.width / 2, myCanvas.width * 0.9);
+const car = new Car(road.getLaneCenter(1), 500, 30, 50);
 
 animate();
 
 function animate() {
-  myCanvas.height = window.innerHeight;
   car.update();
+
+  myCanvas.height = window.innerHeight;
+
+  ctx.translate(0, -car.y + myCanvas.height * 0.7);
+  road.draw(ctx);
   car.draw(ctx);
 
   requestAnimationFrame(animate);
